e03fce5376aacadb166a8f977077a3b9e7464b35
Teo Jia Cheng <jcteo.2018@scis.smu.edu.sg>
2025-02-13 12:39:34 +0800

    threads

--- src/main/java/org/jiacheng/teo/Multithread.java
+++ src/main/java/org/jiacheng/teo/Multithread.java
@@ -2,13 +2,11 @@ package org.jiacheng.teo;
 
 // Main Class
 class Multithread {
-	public static void main(String[] args)
-	{
-		int n = 8; // Number of threads
-		for (int i = 0; i < n; i++) {
-			Thread object
-				= new Thread(new MultithreadingDemo());
-			object.start();
-		}
-	}
+    public static void main(String[] args) {
+        int n = 8; // Number of threads
+        for (int i = 0; i < n; i++) {
+            Thread object = new Thread(new MultithreadingDemo());
+            object.start();
+        }
+    }
 }
--- src/main/java/org/jiacheng/teo/MultithreadingDemo.java
+++ src/main/java/org/jiacheng/teo/MultithreadingDemo.java
@@ -3,17 +3,13 @@ package org.jiacheng.teo;
 // Java code for thread creation by implementing
 // the Runnable Interface
 class MultithreadingDemo implements Runnable {
-	public void run()
-	{
-		try {
-			// Displaying the thread that is running
-			System.out.println(
-				"Thread " + Thread.currentThread().getId()
-					+ " is running");
-		}
-		catch (Exception e) {
-			// Throwing an exception
-			System.out.println("Exception is caught");
-		}
-	}
+    public void run() {
+        try {
+            // Displaying the thread that is running
+            System.out.println("Thread " + Thread.currentThread().getId() + " is running");
+        } catch (Exception e) {
+            // Throwing an exception
+            System.out.println("Exception is caught");
+        }
+    }
 }
